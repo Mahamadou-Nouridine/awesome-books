daa8a0bebab208adea2914e87b8932b73d63118e
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-04-24 22:44:53 +0100

    preserve data and fix linters error

--- script/data.js
+++ script/data.js
@@ -1,13 +1,13 @@
 export default [
-    {
-        id:1,
-        title: 'Atomic Habit',
-        author: 'James Clear'
-    },
-    
-    {
-        id:2,
-        title: 'On Writing Well, ',
-        author: 'James Clear'
-    }
-]
+  {
+    id: 1,
+    title: 'Atomic Habit',
+    author: 'James Clear',
+  },
+
+  {
+    id: 2,
+    title: 'On Writing Well, ',
+    author: 'James Clear',
+  },
+];
--- script/script.js
+++ script/script.js
@@ -1,49 +1,59 @@
-import data from "./data.js";
-const booksContainer = document.querySelector(".books-container");
-const form = document.querySelector('.form')
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
-let books = data;
+import data from './data.js';
+
+const booksContainer = document.querySelector('.books-container');
+const form = document.querySelector('.form');
+const titleEl = document.querySelector('#title');
+const authorEl = document.querySelector('#author');
+
+const localData = localStorage.getItem('books');
+if (!localData) localStorage.setItem('books', JSON.stringify(data));
+let books = JSON.parse(localStorage.getItem('books'));
+
+const saveLocally = () => {
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 class BooksCollection {
-  static add = (title, author, id = Date.now()) => {
-    books.push({title, author, id});
-    console.log(books);
-    loadData()
-  };
+    static add = (title, author, id = Date.now()) => {
+      books.push({ title, author, id });
+      saveLocally();
+      titleEl.value = '';
+      authorEl.value = '';
+    };
 
-  static remove = (id) => {
-    const remainingData = books.filter((book) => book.id != id);
-    books = remainingData;
-    console.log(books);
-    loadData();
-  };
+    static remove = (id) => {
+      const remainingData = books.filter((book) => book.id !== id);
+      books = remainingData;
+      saveLocally();
+    };
 }
 
 const loadData = () => {
-  booksContainer.innerHTML = "";
+  booksContainer.innerHTML = '';
   books.forEach((book) => {
-    const bookEl = document.createElement("div");
-    bookEl.classList.add("book");
+    const bookEl = document.createElement('div');
+    bookEl.classList.add('book');
     bookEl.innerHTML = `<div class="book">
-        <p class="title">${book.title}</p>
-        <p class="author">${book.author}</p>
-        <button  class = 'removeButton-${book.id}' id='${book.id}' >Remove</button>
-        <hr>
-      </div>`;
+          <p class="title">${book.title}</p>
+          <p class="author">${book.author}</p>
+          <button  class = 'removeButton-${book.id}' id='${book.id}' >Remove</button>
+          <hr>
+        </div>`;
 
     booksContainer.appendChild(bookEl);
     const removeButton = document.querySelector(`.removeButton-${book.id}`);
-    removeButton.addEventListener("click", () => {
+    removeButton.addEventListener('click', () => {
       BooksCollection.remove(book.id);
+      loadData();
     });
   });
 };
 
 // add data
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    BooksCollection.add(title.value, author.value)
-})
+  e.preventDefault();
+  BooksCollection.add(titleEl.value, authorEl.value);
+  loadData();
+});
 
 loadData();
